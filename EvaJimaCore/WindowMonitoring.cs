b093e7967c120beb1908574f5e972931c41c7f10
Vitaly Sushilov <Vitaly.Sushilov@clicktale.com>
2017-01-28 11:09:48 +0200

    version 1.28 hotfix 1

--- EvaJimaCore/WindowMonitoring.cs
+++ EvaJimaCore/WindowMonitoring.cs
@@ -112,10 +112,12 @@ namespace EveJimaCore
 
             HideAllContainers();
 
+            ucContainreAuthorization.RefreshAuthorizationStatus();
+
             if (IsNeedUpdateApplication() == false)
             {
                 OpenAuthorizationPanel();
-                ucContainreAuthorization.RefreshAuthorizationStatus();
+                
             }
             else
             {
